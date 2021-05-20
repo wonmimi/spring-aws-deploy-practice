8adb55f745c607f933815e5f943efa8c8003e12c
wonmimi <devwm@naver.com>
2021-05-21 02:07:44 +0900

    03: JPA auditing remove comment

--- src/main/java/com/aws/toy/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/aws/toy/springboot/domain/BaseTimeEntity.java
@@ -10,14 +10,14 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // JPA entity 클래스가 이 클래스를 상속할 경우 저장시간 관련 필드들도 칼럼으로 인식 된다
-@EntityListeners(AuditingEntityListener.class) // 클래스에 Auditing 기능 포함
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
-    @CreatedDate // 엔티티 생성되어 저장될때 시간 자동저장
+    @CreatedDate
     private LocalDateTime createdDate;
 
-    @LastModifiedDate   // 엔티티의 값을 변경할때 자동저장
+    @LastModifiedDate
     private LocalDateTime modifiedDate;
 
 }
