ed3bd80bd674d64d04d1bf1c87fca7faa274b984
wonmimi <devwm@naver.com>
2021-05-23 23:22:41 +0900

    04 게시글 등록 , 전체 글 조회 기능 및 화면

--- src/main/java/com/aws/toy/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/aws/toy/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,12 @@
 package com.aws.toy.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // = Repository  , Entity (Posts) 와 함께 위치해야한다 ( Mybatis: Dao)
 public  interface PostsRepository extends JpaRepository<Posts, Long> {  // <Entity클래스, PK타입>
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
