c44698b36d13ebe88e572e09de9b12da376521b1
wonmimi <devwm@naver.com>
2021-05-23 23:59:54 +0900

    04 게시글 수정 기능 및 화면

--- src/main/java/com/aws/toy/springboot/web/IndexController.java
+++ src/main/java/com/aws/toy/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.aws.toy.springboot.web;
 
 import com.aws.toy.springboot.service.posts.PostsService;
+import com.aws.toy.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +22,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,9 +1,17 @@
 var main = {
     init : function () {
         var _this = this;
+
+        //등록
         $('#btn-save').on('click', function() {
             _this.save();
         });
+
+        //수정
+        $('#btn-update').on('click',function(){
+            _this.update();
+        });
+
     },
     save : function() {
         var data = {
@@ -24,6 +32,29 @@ var main = {
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
+
+    },
+
+    update : function(){
+        var data = {
+            title : $('#title').val(),
+            content : $('#content').val()
+        };
+        var id = $('#id').val();
+
+         $.ajax({
+            type:'PUT',
+            url:'/api/v1/posts/'+id,
+            dataType:'json',
+            contentType:'application/json; charset-uyf-8',
+            data: JSON.stringify(data),
+        }).done(function(){
+            alert('글이 수정 되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+
     }
 };
 
